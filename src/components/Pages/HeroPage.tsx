d6bef0dc273272a4317e11f29c798130e700575d
Frederik Graakjær <frede.flip@gmail.com>
2024-12-05 15:31:57 +0100

    heropage test

--- src/components/Pages/HeroPage.tsx
+++ src/components/Pages/HeroPage.tsx
@@ -1,24 +1,12 @@
 import React from "react";
 import {
-  FlexibleSectionsFlexContentHeroPageSection2Layout,
-  FlexibleSectionsFlexContentHeroPageSection3Layout,
-  FlexibleSectionsFlexContentInformationSectionLayout,
-  FlexibleSectionsFlexContentHeroPageSection5Layout,
-  FlexibleSectionsFlexContentHeroSectionLayout,
   FlexibleSectionsFlexContentLayout,
   GlobalFlexibleSectionsSectionsInfiniteSliderSectionLayout,
   GlobalSections,
-  FlexibleSectionsFlexContentMoreBlogsSectionLayout,
   BlogConnection,
 } from "@/gql/graphql";
-import HeroSection from "../Sections/HeroPage/HeroSection";
 import SliderSection from "../Sections/SliderSection";
-import Section2 from "../Sections/HeroPage/Section2";
-import Section3 from "../Sections/HeroPage/Section3";
-import InformationSection from "../Sections/InformationSection";
-import Section5 from "../Sections/HeroPage/Section5";
 import Divider from "../ui/divider";
-import ReadMoreBlogsSection from "../Sections/ReadMoreBlogsSection";
 import { renderSections } from "@/utils/renderSections";
 
 interface HeroPageProps {
@@ -31,12 +19,6 @@ const HeroPage: React.FC<HeroPageProps> = ({
   globalSections,
   atMistePosts,
 }) => {
-  console.log("sections", sections);
-  // const heroSection = sections.find(
-  //   (section) =>
-  //     section.fieldGroupName === "FlexibleSectionsFlexContentHeroSectionLayout"
-  // ) as FlexibleSectionsFlexContentHeroSectionLayout;
-
   const infiniteSliderSection =
     globalSections.globalFlexibleSections?.sections?.find(
       (section) =>
@@ -44,38 +26,8 @@ const HeroPage: React.FC<HeroPageProps> = ({
         "GlobalFlexibleSectionsSectionsInfiniteSliderSectionLayout"
     ) as GlobalFlexibleSectionsSectionsInfiniteSliderSectionLayout;
 
-  // const section2 = sections.find(
-  //   (section) =>
-  //     section.fieldGroupName ===
-  //     "FlexibleSectionsFlexContentHeroPageSection2Layout"
-  // ) as FlexibleSectionsFlexContentHeroPageSection2Layout;
-
-  // const section3 = sections.find(
-  //   (section) =>
-  //     section.fieldGroupName ===
-  //     "FlexibleSectionsFlexContentHeroPageSection3Layout"
-  // ) as FlexibleSectionsFlexContentHeroPageSection3Layout;
-
-  // const informationSection = sections.find(
-  //   (section) =>
-  //     section.fieldGroupName ===
-  //     "FlexibleSectionsFlexContentInformationSectionLayout"
-  // ) as FlexibleSectionsFlexContentInformationSectionLayout;
-
-  // const section5 = sections.find(
-  //   (section) =>
-  //     section.fieldGroupName ===
-  //     "FlexibleSectionsFlexContentHeroPageSection5Layout"
-  // ) as FlexibleSectionsFlexContentHeroPageSection5Layout;
-
-  // const readBlogs = sections?.find(
-  //   (section) =>
-  //     section.fieldGroupName ===
-  //     "FlexibleSectionsFlexContentMoreBlogsSectionLayout"
-  // ) as FlexibleSectionsFlexContentMoreBlogsSectionLayout;
-
   return (
-    <div className="">
+    <div>
       {renderSections(sections, { atMistePosts })}
       <Divider />
       <SliderSection section={infiniteSliderSection} />
